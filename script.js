fba591809422860159d0ba40ea3dcb4b669dfaa4
mlopez0709 <156136097+mlopez0709@users.noreply.github.com>
2024-02-01 15:10:19 -0500

    Update HTML content

---
+++ script.js
@@ -0,0 +1,13 @@
+// alert("Mariana");
+//alert("Lopez");
+//alert("18")
+ console.log("Mariana ")
+ console.log("Lopez")
+ console.log("18")
+ var firstName ="Mariana";
+ var age = 18;
+ var lastName = "Lopez";
+ alert ('My name is ' +  firstName + '' + lastName + '.') ;
+ alert('I have ' +  age  + ' years old.');
+
+console.log('Having so much fun in the class!');
